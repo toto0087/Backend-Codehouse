11c79e11e85e6fae57dd50af1593a8553353d46c
Tobias <tobiasocchi03@hotmail.com>
2023-10-27 19:29:08 -0300

    cookies config

--- src/app.js
+++ src/app.js
@@ -4,11 +4,16 @@ import viewsRouter from "./router/viewsRouter.js"
 import chatRouter from "./router/chatRouter.js"
 import __dirname from "./utils.js";
 import "./db/config.js"
+import FileStore from "session-file-store";
+import cookieParser from "cookie-parser";
+import MongoStore from "connect-mongo";
+import session from "express-session";
 
 import express from "express";
 import { Server } from "socket.io";
 import handlebars from "express-handlebars";
 import { productsManager } from "./dao/db/productsManager.js";
+import { URI } from "./db/config.js";
 const app = express();
 const port = 3000; 
 
@@ -17,7 +22,6 @@ app.use("/api/products",productsRouter)
 app.use("/api/carts",cartsRotuer)
 app.use('/', viewsRouter)
 app.use('/api/chat', chatRouter)
-
 
 // Inicializacion de motor de plantillas
 app.engine("handlebars", handlebars.engine());
@@ -32,7 +36,8 @@ app.set("view engine", "handlebars");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
-
+app.use(cookieParser('123456'));
+app.use(session({ store: MongoStore.create({mongoUrl:URI})}))
 
 // Inicia el servidor en el puerto especificado
 const httpServer = app.listen(port, () => {
--- src/db/config.js
+++ src/db/config.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
-const URI = 'mongodb+srv://totojajaxd:L2ohuAeL8pBfk49n@cluster0.ekq4i4y.mongodb.net/ecommerce?retryWrites=true&w=majority'
+export const URI = 'mongodb+srv://totojajaxd:L2ohuAeL8pBfk49n@cluster0.ekq4i4y.mongodb.net/ecommerce?retryWrites=true&w=majority'
 mongoose
     .connect(URI)
     .then(db => console.log('DB is connected'))
     .catch(err => console.error(err)); 
+
